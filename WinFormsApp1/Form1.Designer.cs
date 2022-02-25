e0b6762bdf6c2879be8d9539393299efefe02100
ArielCristoHaryono <aharyono01@student.ciputra.ac.id>
2022-02-25 10:39:01 +0700

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblNama = new System.Windows.Forms.Label();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.BtnNew = new System.Windows.Forms.Button();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.TxtBoxNama = new System.Windows.Forms.TextBox();
+            this.LblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LblNama
+            // 
+            this.LblNama.AutoSize = true;
+            this.LblNama.Location = new System.Drawing.Point(41, 97);
+            this.LblNama.Name = "LblNama";
+            this.LblNama.Size = new System.Drawing.Size(59, 25);
+            this.LblNama.TabIndex = 0;
+            this.LblNama.Text = "Nama";
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.Location = new System.Drawing.Point(396, 385);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(112, 34);
+            this.BtnSave.TabIndex = 1;
+            this.BtnSave.Text = "Save";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // BtnNew
+            // 
+            this.BtnNew.Location = new System.Drawing.Point(527, 385);
+            this.BtnNew.Name = "BtnNew";
+            this.BtnNew.Size = new System.Drawing.Size(112, 34);
+            this.BtnNew.TabIndex = 2;
+            this.BtnNew.Text = "New";
+            this.BtnNew.UseVisualStyleBackColor = true;
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.BtnClose.Location = new System.Drawing.Point(658, 385);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(112, 34);
+            this.BtnClose.TabIndex = 3;
+            this.BtnClose.Text = "Close";
+            this.BtnClose.UseVisualStyleBackColor = true;
+            this.BtnClose.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TxtBoxNama
+            // 
+            this.TxtBoxNama.Location = new System.Drawing.Point(128, 94);
+            this.TxtBoxNama.Name = "TxtBoxNama";
+            this.TxtBoxNama.Size = new System.Drawing.Size(150, 31);
+            this.TxtBoxNama.TabIndex = 4;
+            this.TxtBoxNama.TextChanged += new System.EventHandler(this.TxtBoxNama_TextChanged);
+            // 
+            // LblOutput
+            // 
+            this.LblOutput.AutoSize = true;
+            this.LblOutput.Location = new System.Drawing.Point(128, 162);
+            this.LblOutput.Name = "LblOutput";
+            this.LblOutput.Size = new System.Drawing.Size(159, 25);
+            this.LblOutput.TabIndex = 5;
+            this.LblOutput.Text = "{...}                         ";
+            this.LblOutput.Click += new System.EventHandler(this.LblOutput_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblOutput);
+            this.Controls.Add(this.TxtBoxNama);
+            this.Controls.Add(this.BtnClose);
+            this.Controls.Add(this.BtnNew);
+            this.Controls.Add(this.BtnSave);
+            this.Controls.Add(this.LblNama);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblNama;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Button BtnNew;
+        private System.Windows.Forms.Button BtnClose;
+        private System.Windows.Forms.TextBox TxtBoxNama;
+        private System.Windows.Forms.Label LblOutput;
+    }
+}
